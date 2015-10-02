6e7d1bd043e0dbc07da6441b639a68a5a510a49f
sukeshni <sukeshni.kantrod@gmail.com>
2015-10-02 16:12:19 +0900

    Mod README.md, Bug Fix

--- src/fizzbuzz.cs
+++ src/fizzbuzz.cs
@@ -2,22 +2,18 @@ using System;
 
 public class FizzBuzz {
 
-  public static object isFizzBuzz(int n) {
+  public static object fizzbuzz(int n) {
     //ここに素数判定プログラムを実装してください。
     //BEGIN_CHALLENGE
 
-    object[] array = new object[101];
+    object data;
 
-    for(int i = 1; i <= 100; i++) {
+    if(n%15 == 0) data = "FizzBuzz";
+    else if(n%5 == 0) data = "Buzz";
+    else if(n%3 == 0) data = "Fizz";
+    else data = n;
 
-      if(i%15 == 0) array[i] = "FizzBuzz";
-      else if(i%5 == 0) array[i] = "Buzz";
-      else if(i%3 == 0) array[i] = "Fizz";
-      else array[i] = i;
-      //Console.WriteLine (array[i]);
-    }
-
-    return array[n];
+    return data;
     //END_CHALLENGE
   }
 
--- src/fizzbuzztest.cs
+++ src/fizzbuzztest.cs
@@ -5,15 +5,17 @@ using System;
 public class FizzBuzzTest {
 
     [Test]
-    public void IsFizzBuzzTest() {
+    public void fizzbuzzTest() {
     
     	for(int i = 1; i <= 100; i++) {
     		if(i%15 == 0) 
-	        	Assert.AreEqual("FizzBuzz", FizzBuzz.isFizzBuzz(i));
+	        	Assert.AreEqual("FizzBuzz", FizzBuzz.fizzbuzz(i));
        		else if (i%5 == 0) 
-	        	Assert.AreEqual("Buzz", FizzBuzz.isFizzBuzz(i));
+	        	Assert.AreEqual("Buzz", FizzBuzz.fizzbuzz(i));
         	else if(i%3 == 0) 
-	        	Assert.AreEqual("Fizz", FizzBuzz.isFizzBuzz(i));
+	        	Assert.AreEqual("Fizz", FizzBuzz.fizzbuzz(i));
+            else 
+                Assert.AreEqual(i, FizzBuzz.fizzbuzz(i));
 	    }
 	}
 
