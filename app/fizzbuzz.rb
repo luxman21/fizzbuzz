f41d3584b2ce1de9c01e04a0db505c1592a4976d
sukeshni kantrod <sukeshnikantrod@sukeshnis-MacBook-Pro.local>
2015-10-26 20:02:59 +0900

    Indentation

--- app/fizzbuzz.rb
+++ app/fizzbuzz.rb
@@ -1,18 +1,18 @@
 def fizzbuzz(n)
-	#ここに素数を一文字づつ数列化した際のn番目の数値を出力するプログラムを実装してください。
-	#BEGIN_CHALLENGE
+  #ここに素数を一文字づつ数列化した際のn番目の数値を出力するプログラムを実装してください。
+  #BEGIN_CHALLENGE
 
-	result = ""
-	if (n% 15 == 0)
-		result = "FizzBuzz"
-	elsif (n % 5 == 0)
-		result = "Buzz"
-	elsif (n % 3 == 0)
-		result = "Fizz"
-	else result = n 
-	end 
+  result = ""
+  if (n% 15 == 0)
+    result = "FizzBuzz"
+  elsif (n % 5 == 0)
+    result = "Buzz"
+  elsif (n % 3 == 0)
+    result = "Fizz"
+  else result = n 
+  end 
 
-	return result
+  return result
 
-	#END_CHALLENGE
+  #END_CHALLENGE
 end
--- test/fizzbuzztest.rb
+++ test/fizzbuzztest.rb
@@ -2,21 +2,21 @@ require_relative '../app/fizzbuzz'
 
 expected = []
 100.times do |i|
-	if (i% 15 == 0)
-		expected.push("FizzBuzz")
-	elsif (i % 5 == 0)
-		expected.push("Buzz")
-	elsif (i % 3 == 0)
-		expected.push("Fizz")
-	else expected.push(i) 	
-	end
-end		
+  if (i% 15 == 0)
+    expected.push("FizzBuzz")
+  elsif (i % 5 == 0)
+    expected.push("Buzz")
+  elsif (i % 3 == 0)
+    expected.push("Fizz")
+  else expected.push(i)   
+  end
+end   
 
 describe "fizzbuzz" do
-	
-	it "1 to 100" do
-		100.times do |i|
-			expect(fizzbuzz(i)).to eq(expected[i])
-		end
-	end	
+  
+  it "1 to 100" do
+    100.times do |i|
+      expect(fizzbuzz(i)).to eq(expected[i])
+    end
+  end 
 end
