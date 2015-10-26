afe6966e5d966af4750d45a4e79c66e589a05627
sukeshni kantrod <sukeshnikantrod@sukeshnis-MacBook-Pro.local>
2015-10-26 15:22:21 +0900

    Indentation

--- app/fizzbuzz.js
+++ app/fizzbuzz.js
@@ -1,6 +1,6 @@
 function fizzbuzz(data) {
 	//BEGIN_CHALLENGE
-	var result = [];
+	var result;
 	if(data % 15 == 0) { result = "FizzBuzz"; }
 	else if(data % 5 == 0) {result = "Buzz"; }
 	else if(data %3 ==0 ) {result = "Fizz"; }
