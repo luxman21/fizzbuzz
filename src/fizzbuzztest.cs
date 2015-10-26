aa8ea173adb70028df62ca0d22233dc9e57c3abf
sukeshni kantrod <sukeshnikantrod@sukeshnis-MacBook-Pro.local>
2015-10-26 15:30:09 +0900

    Indentation

--- src/fizzbuzztest.cs
+++ src/fizzbuzztest.cs
@@ -4,19 +4,19 @@ using System;
 [TestFixture]
 public class FizzBuzzTest {
 
-    [Test]
-    public void fizzbuzzTest() {
+  [Test]
+  public void fizzbuzzTest() {
     
-    	for(int i = 1; i <= 100; i++) {
-    		if(i%15 == 0) 
-	        	Assert.AreEqual("FizzBuzz", FizzBuzz.fizzbuzz(i));
-       		else if (i%5 == 0) 
-	        	Assert.AreEqual("Buzz", FizzBuzz.fizzbuzz(i));
-        	else if(i%3 == 0) 
-	        	Assert.AreEqual("Fizz", FizzBuzz.fizzbuzz(i));
-            else 
-                Assert.AreEqual(i, FizzBuzz.fizzbuzz(i));
-	    }
+  	for(int i = 1; i <= 100; i++) {
+  		if(i%15 == 0) 
+        	Assert.AreEqual("FizzBuzz", FizzBuzz.fizzbuzz(i));
+     		else if (i%5 == 0) 
+        	Assert.AreEqual("Buzz", FizzBuzz.fizzbuzz(i));
+      	else if(i%3 == 0) 
+        	Assert.AreEqual("Fizz", FizzBuzz.fizzbuzz(i));
+        else 
+          Assert.AreEqual(i, FizzBuzz.fizzbuzz(i));
+    }
 	}
 
 }
