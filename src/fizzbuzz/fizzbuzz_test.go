c0d9e3b1aa020dec9db01629d636318e3a59ea52
sukeshni kantrod <sukeshnikantrod@sukeshnis-MacBook-Pro.local>
2015-10-27 16:54:02 +0900

    Indentation

--- src/fizzbuzz/fizzbuzz_test.go
+++ src/fizzbuzz/fizzbuzz_test.go
@@ -4,23 +4,23 @@ import "testing"
 
 func TestFizzBuzz(t *testing.T) {
 
-	var expected [101]string
+  var expected [101]string
   var actual [101]string
 
-	for i:=1;i<=100;i++ {
+  for i:=1;i<=100;i++ {
 
     actual[i] = fizzBuzz(i)
-		if(i % 15 == 0) {
-			expected[i] := "FizzBuzz"
-		}
-		else if(i % 5 == 0) {
-			expected[i] := "Buzz"
-		} 
-		else if(i % 3 == 0) {
-			expected[i] := "Fizz"
-		}
-		else expected[i] := strconv.Itoa(n)
-	}
+    if(i % 15 == 0) {
+      expected[i] := "FizzBuzz"
+    }
+    else if(i % 5 == 0) {
+      expected[i] := "Buzz"
+    } 
+    else if(i % 3 == 0) {
+      expected[i] := "Fizz"
+    }
+    else expected[i] := strconv.Itoa(n)
+  }
 
   if actual != expected {
     t.Errorf("got %v\nwant %v", actual, expected)
