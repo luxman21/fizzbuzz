0b7be2307d7f997a74758bbe3622437a36c0eba0
sukeshni kantrod <sukeshnikantrod@sukeshnis-MacBook-Pro.local>
2015-10-28 11:10:44 +0900

    FizzBuzz- go done

--- src/fizzbuzz/fizzbuzz.go
+++ src/fizzbuzz/fizzbuzz.go
@@ -6,17 +6,16 @@ func fizzBuzz(n int) string {
   //ここに素数を一文字づつ数列化した際のn番目の数値を出力するプログラムを実装してください。
   //BEGIN_CHALLENGE
 
-  var string result;
+  var result string
   if(n % 15 == 0) {
-    result := "FizzBuzz"
+    result = "FizzBuzz"
+  } else if(n % 5 == 0) {
+    result = "Buzz"
+  } else if(n % 3 == 0) {
+    result = "Fizz"
+  } else {
+    result = strconv.Itoa(n)
   }
-  else if(n % 5 == 0) {
-    result := "Buzz"
-  } 
-  else if(n % 3 == 0) {
-    result := "Fizz"
-  }
-  else result := strconv.Itoa(n)
   return result
   //END_CHALLENGE
 }
--- src/fizzbuzz/fizzbuzz_test.go
+++ src/fizzbuzz/fizzbuzz_test.go
@@ -1,6 +1,7 @@
 package fizzbuzz
 
 import "testing"
+import "strconv"
 
 func TestFizzBuzz(t *testing.T) {
 
@@ -8,18 +9,15 @@ func TestFizzBuzz(t *testing.T) {
   var actual [101]string
 
   for i:=1;i<=100;i++ {
-
+    
     actual[i] = fizzBuzz(i)
     if(i % 15 == 0) {
-      expected[i] := "FizzBuzz"
-    }
-    else if(i % 5 == 0) {
-      expected[i] := "Buzz"
-    } 
-    else if(i % 3 == 0) {
-      expected[i] := "Fizz"
-    }
-    else expected[i] := strconv.Itoa(n)
+      expected[i] = "FizzBuzz"
+    } else if(i % 5 == 0) {
+      expected[i] = "Buzz"
+    } else if(i % 3 == 0) {
+      expected[i] = "Fizz"
+    } else { expected[i] = strconv.Itoa(i) }
   }
 
   if actual != expected {
