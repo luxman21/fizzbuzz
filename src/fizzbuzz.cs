8986a3f5881963e02d3ce00c521b3bc8c368577b
NckHmml <NckHmml@users.noreply.github.com>
2015-10-28 16:35:29 +0900

    Refractored C# code

--- src/fizzbuzz.cs
+++ src/fizzbuzz.cs
@@ -1,19 +1,21 @@
 using System;
 
-public class FizzBuzz {
+public class FizzBuzz
+{
 
-  public static string FizzBuzz(int n) {
-    //ここに素数判定プログラムを実装してください。
-    //BEGIN_CHALLENGE
-    
-    string data;    
-    if (n % 15 == 0) data = "FizzBuzz";
-    else if(n % 5 == 0) data = "Buzz";
-    else if(n % 3 == 0) data = "Fizz";
-    else data = n.ToString();
+    public static string DoFizzBuzz(int n)
+    {
+        //ここに素数判定プログラムを実装してください。
+        //BEGIN_CHALLENGE
 
-    return data;
-    //END_CHALLENGE
-  }
+        string data;
+        if (n % 15 == 0) data = "FizzBuzz";
+        else if (n % 5 == 0) data = "Buzz";
+        else if (n % 3 == 0) data = "Fizz";
+        else data = n.ToString();
+
+        return data;
+        //END_CHALLENGE
+    }
 
 }
--- src/fizzbuzztest.cs
+++ src/fizzbuzztest.cs
@@ -2,20 +2,23 @@ using NUnit.Framework;
 using System;
 
 [TestFixture]
-public class FizzBuzzTest {
+public class FizzBuzzTest
+{
 
-  [Test]
-  public void FizzBuzzTest() {
-    
-    for(int i = 1; i <= 100; i++) {
-      if(i%15 == 0) 
-        Assert.AreEqual("FizzBuzz", FizzBuzz.FizzBuzz(i));
-      else if (i%5 == 0) 
-        Assert.AreEqual("Buzz", FizzBuzz.FizzBuzz(i));
-      else if(i%3 == 0) 
-        Assert.AreEqual("Fizz", FizzBuzz.FizzBuzz(i));
-      else 
-        Assert.AreEqual(i.ToString(), FizzBuzz.FizzBuzz(i));
+    [Test]
+    public void DoFizzBuzzTest()
+    {
+
+        for (int i = 1; i <= 100; i++)
+        {
+            if (i % 15 == 0)
+                Assert.AreEqual("FizzBuzz", FizzBuzz.DoFizzBuzz(i));
+            else if (i % 5 == 0)
+                Assert.AreEqual("Buzz", FizzBuzz.DoFizzBuzz(i));
+            else if (i % 3 == 0)
+                Assert.AreEqual("Fizz", FizzBuzz.DoFizzBuzz(i));
+            else
+                Assert.AreEqual(i.ToString(), FizzBuzz.DoFizzBuzz(i));
+        }
     }
-  }
 }
